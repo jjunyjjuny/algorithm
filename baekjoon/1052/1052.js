149ae4ee97c27f0d9ffbf5df7662210580990517
jjunyjjuny <jjuny546@gmail.com>
2021-01-06 16:12:51 +0900

    Add 1009, 1052, 1076(진행 중)

---
+++ baekjoon/1052/1052.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+// const stdin = fs.readFileSync("/dev/stdin", "utf8");
+const stdin = fs.readFileSync("./stdin").toString().split(" ");
+const stdout = fs.readFileSync("./stdout").toString().split(" ");
+
+// stdin, stdout을 필요한 형태로 변형(숫자로 바꾸거나 배열로 바꾸거나 등)
+const inputs = stdin.map((el) => +el);
+const answer = stdout;
+// console.log(inputs);
+
+const arr = [];
+// while (inputs % 2 !== 0) {}
+// 정답으로 제출할 내용을 이곳에 모은다.
+const result = [];
+// 실제 정답 코드 작성란
+
+// 코드 작성 중 테스트
+// testcase(result, answer);
+
+// 정답 제출
+// result.forEach((ans) => console.log(ans));
+
+function testcase(result, stdout) {
+  result.forEach((ans, index) => {
+    if (ans === stdout[index]) console.log(`${index} case PASS !!!`);
+    else console.log(`${index} case FAIL !!! `);
+  });
+}
+
+function findMax(n) {
+  let remind = n;
+  let count = 0;
+  while (remind >= 2) {
+    count++;
+    remind = remind / 2;
+  }
+  return 2 ** count;
+}
+
+console.log(findMax(8));
