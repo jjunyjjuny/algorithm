04f3a82770b5ec71791056caa41517a0dbf62e4a
jjunyjjuny <jjuny546@gmail.com>
2021-04-23 23:30:28 +0900

    Add happy_number

---
+++ leetCode/happy_number/solution.js
@@ -0,0 +1,21 @@
+var isHappy = function (n) {
+  const record = {};
+  let currentNum = getNext(n);
+  do {
+    if (currentNum === 1) {
+      return true;
+    } else {
+      record[currentNum] = 1;
+      currentNum = getNext(currentNum);
+    }
+  } while (!(currentNum in record));
+
+  return false;
+};
+
+function getNext(n) {
+  const array = String(n).split("");
+  return array.reduce((acc, curr) => {
+    return acc + Number(curr) ** 2;
+  }, 0);
+}
